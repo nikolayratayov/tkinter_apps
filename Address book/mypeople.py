ece17cf0f4d442d989564e3a850c66b296fd2fd4
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-03-13 16:41:47 +0200

    Address book

--- Address book/mypeople.py
+++ Address book/mypeople.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import sqlite3
 import addperson
+from tkinter import messagebox
 
 
 con = sqlite3.connect('database.db')
@@ -71,7 +72,7 @@ class MyPeople(Toplevel):
 class Update(Toplevel):
     def __init__(self):
         Toplevel.__init__(self)
-        self.geometry('650x750+550+100')
+        self.geometry('650x650+550+100')
         self.title('Update person')
         self.resizable(False, False)
 
@@ -84,3 +85,70 @@ class Update(Toplevel):
         self.person_email = person_info[0][3]
         self.person_phone = person_info[0][4]
         self.person_address = person_info[0][5]
+
+        self.top = Frame(self, height=150, bg='white')
+        self.top.pack(fill=X)
+        self.bottom = Frame(self, height=500, bg='#fcc324')
+        self.bottom.pack(fill=X)
+
+        # Heading, image and date
+        self.top_image = PhotoImage(file='icons/addperson.png')
+        self.top_image_lbl = Label(self.top, image=self.top_image, bg='white')
+        self.top_image_lbl.place(x=120, y=10)
+        self.heading = Label(self.top, text='My People', font='arial 15 bold', fg='#003f8a', bg='white')
+        self.heading.place(x=260, y=60)
+
+        # Labels and entries
+        self.lbl_name = Label(self.bottom, text='Name', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_name.place(x=40, y=40)
+        self.ent_name = Entry(self.bottom, width=30, bd=4)
+        self.ent_name.insert(0, self.person_name)
+        self.ent_name.place(x=150, y=45)
+
+        self.lbl_surname = Label(self.bottom, text='Surname', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_surname.place(x=40, y=80)
+        self.ent_surname = Entry(self.bottom, width=30, bd=4)
+        self.ent_surname.insert(0, self.person_surname)
+        self.ent_surname.place(x=150, y=85)
+
+        self.lbl_email = Label(self.bottom, text='Email', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_email.place(x=40, y=120)
+        self.ent_email = Entry(self.bottom, width=30, bd=4)
+        self.ent_email.insert(0, self.person_email)
+        self.ent_email.place(x=150, y=125)
+
+        self.lbl_phone = Label(self.bottom, text='Phone', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_phone.place(x=40, y=160)
+        self.ent_phone = Entry(self.bottom, width=30, bd=4)
+        self.ent_phone.insert(0, self.person_phone)
+        self.ent_phone.place(x=150, y=165)
+
+        self.lbl_address = Label(self.bottom, text='Address', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_address.place(x=40, y=300)
+        self.address = Text(self.bottom, width=23, height=15, wrap=WORD)
+        self.address.insert('1.0', self.person_address)
+        self.address.place(x=150, y=200)
+
+        button = Button(self.bottom, text='Update person', command=self.update_person)
+        button.place(x=270, y=460)
+
+    def update_person(self):
+        person_id = self.person_id
+        person_name = self.ent_name.get()
+        person_surname = self.ent_surname.get()
+        person_email = self.ent_email.get()
+        person_phone = self.ent_phone.get()
+        person_address = self.address.get(1.0, 'end-1c')
+
+        if person_name and person_surname and person_email and person_phone and person_address:
+            try:
+                query = "UPDATE people set person_name=?, person_surname=?, person_email=?, person_phone=?, person_address=?" \
+                        " WHERE person_id=?"
+                cur.execute(query, (person_name, person_surname, person_email, person_phone, person_address, person_id))
+                con.commit()
+                messagebox.showinfo('Success', 'Person has been updated!')
+                self.destroy()
+            except:
+                messagebox.showinfo('Warning', 'Person has not been updated!', icon='warning')
+        else:
+            messagebox.showinfo('Warning', 'Fields can\'t be empty!', icon='warning')
