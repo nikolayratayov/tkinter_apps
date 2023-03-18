2a881026a0eec2f63f769a1c2c5b79d6ad56907d
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-03-18 08:53:52 +0200

    Address book

--- Library management system/main.py
+++ Library management system/main.py
@@ -11,7 +11,7 @@ class Main:
         main_frame.pack()
         # top frame
         top_frame = Frame(main_frame, width=1350, height=70, bg='#f8f8f8', relief=SUNKEN, borderwidth=3)
-        top_frame.pack()
+        top_frame.pack(fill=X)
         # center frame
         center_frame = Frame(main_frame, width=1350, height=575, relief=RIDGE, bg='#e0f0f0', borderwidth=3)
         center_frame.pack()
@@ -21,6 +21,25 @@ class Main:
         # center_right_frame
         center_right_frame = Frame(center_frame, width=450, height=575, bg='#e0f0f0', borderwidth=3, relief=SUNKEN)
         center_right_frame.pack()
+        # search bar
+        search_bar = LabelFrame(center_right_frame, width=440, height=75, text='Search box', bg='#9bc9ff')
+        search_bar.pack()
+        # list bar
+        list_bar = LabelFrame(center_right_frame, width=440, height=175, text='List box', bg='#fcc324')
+        list_bar.pack()
+        # add book
+        self.icon_book = PhotoImage(file='icons/add_book.png')
+        self.btn_book = Button(top_frame, text='Add Book', image=self.icon_book, compound=LEFT, font='arial 12 bold')
+        self.btn_book.pack(side=LEFT)
+        # add member
+        self.icon_member = PhotoImage(file='icons/users.png')
+        self.btn_member = Button(top_frame, text='Add Member', font='arial 12 bold', padx=10)
+        self.btn_member.configure(image=self.icon_member, compound=LEFT)
+        self.btn_member.pack(side=LEFT)
+        # give book
+        self.icon_give = PhotoImage(file='icons/givebook.png')
+        self.btn_give = Button(top_frame, text='Give Book', image=self.icon_give, compound=LEFT, font='arial 12 bold')
+        self.btn_give.pack(side=LEFT)
 
 
 def main():
