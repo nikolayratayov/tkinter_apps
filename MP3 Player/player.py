eac39908a99461b7ed8d9104e722d7f5c28a8bec
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-04-02 17:23:15 +0300

    MP3 player

--- MP3 Player/player.py
+++ MP3 Player/player.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+import pygame
+
+
+pygame.mixer.init()
 
 
 def add_song():
@@ -26,6 +30,30 @@ def delete_all_songs():
     playlist_box.delete(0, END)
 
 
+def play():
+    song = playlist_box.get(ACTIVE)
+    song = f'D:/GitHub/tkinter_apps/MP3 Player/audio/{song}.mp3'
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+
+
+def stop():
+    pygame.mixer.music.stop()
+
+
+def pause(is_paused):
+    global paused
+    paused = is_paused
+    if paused:
+        paused = False
+        pygame.mixer.music.unpause()
+    else:
+        paused = True
+        pygame.mixer.music.pause()
+
+
+paused = False
+
 root = Tk()
 root.title('MP3 Player')
 root.geometry('500x400+500+100')
@@ -47,11 +75,11 @@ back_button = ttk.Button(control_frame, image=back_image)
 back_button.grid(row=0, column=0, padx=10)
 forward_button = ttk.Button(control_frame, image=forward_image)
 forward_button.grid(row=0, column=1, padx=10)
-play_button = ttk.Button(control_frame, image=play_image)
+play_button = ttk.Button(control_frame, image=play_image, command=play)
 play_button.grid(row=0, column=2, padx=10)
-pause_button = ttk.Button(control_frame, image=pause_image)
+pause_button = ttk.Button(control_frame, image=pause_image, command=lambda: pause(paused))
 pause_button.grid(row=0, column=3, padx=10)
-stop_button = ttk.Button(control_frame, image=stop_image)
+stop_button = ttk.Button(control_frame, image=stop_image, command=stop)
 stop_button.grid(row=0, column=4, padx=10)
 
 my_menu = Menu(root)
